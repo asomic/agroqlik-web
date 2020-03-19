8485deeffadaa8289c5857ad82abfca1915d931a
raul.morales <50383306+0113moralessssss@users.noreply.github.com>
2020-03-19 13:59:38 +0100

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,7 +12,7 @@
     <link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{asset('/favicons/apple-touch-icon-57x57.png')}}" />
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{asset('/favicons/apple-touch-icon-114x114.png')}}" />
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{asset('/favicons/apple-touch-icon-72x72.png')}}" />
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{asset(''favicons/apple-touch-icon-144x144.png)}}/" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{asset('/favicons/apple-touch-icon-144x144.png')}}" />
     <link rel="apple-touch-icon-precomposed" sizes="60x60" href="{{asset('/favicons/apple-touch-icon-60x60.png')}}" />
     <link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{asset('/favicons/apple-touch-icon-120x120.png')}}" />
     <link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{asset('/favicons/apple-touch-icon-76x76.png')}}" />
